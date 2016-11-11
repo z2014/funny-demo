079322b200eb267839e5d000452598f9c01496f2
z2014 <591763389@qq.com>
2016-11-11 20:49:16 +0800

    坦克大战

--- tank/js/tankFactory.js
+++ tank/js/tankFactory.js
@@ -21,40 +21,96 @@ function createTank(factory) {
 			    				tankDiv.style.top = y + 5 + 'px';
 			    			}	
 			    	}else{
-			    		factory.removeChild(tankDiv);
-			    		clearInterval(moveTimer);
+			    		if (x < 540) {
+			    			tankDiv.style.left = x + 5 + 'px';
+			    		}else{
+			    			clearInterval(moveTimer);
+			    			factory.removeChild(tankDiv);
+			    			tankDiv = null;
+			    		}
 			    	}
 		    	},50);
 		    }
-		    tankDiv.fire = function() {
-            	// console.log('fire');
-            	// var fireTimer = setInterval(function() {
-            		createBullet(factory,tankDiv);
-            		console.log(2);
-            	// },1000)
-		    };
+		    tankDiv.fire = function(yourtank) {
+		    	var fireTimer = setInterval(function() {
+		    		console.log(1);
+	            	if (yourtank) {
+	            		createBullet(factory,tankDiv);
+	            	}
+	            	else{
+	            		clearInterval(fireTimer);
+	            	}
+	            	// new Promise(function(resolve){
+	            	// 	resolve(function(){
+	            	// 		return value == false;
+	            	// 	});
+	            	// }).then(clearInterval(fireTimer));
+			    },1000);
+		    }
 		    factory.appendChild(tankDiv);
-		    setTimeout(function() {
-		    	console.log(1);
-		    	var tank1 = createTank(center);
-		    	// tank1.run();
-		    	// tank1.fire();
-		    },500);
 		    return tankDiv;
 	    }
     };
     return tank;
 }
 function createBullet(factory,tankDiv) {
-	var bullet = document.createElement('div');
-	bullet.className = 'ermybullet';
-	bullet.style.top =  tankDiv.offsetTop + '40px';
-	bullet.style.left = tankDiv.offsetLeft + '15px';
-	factory.appendChild(bullet);
-	var timer3 = setInterval(function() {
-		bullet.style.top = bullet.offsetTop + 10 + 'px';
-		if (bullet.offsetTop > 560) {
-			clearInterval(timer3);
-		}
-	},50);
+	if (tankDiv.offsetTop < 540) {
+		var bullet = document.createElement('div');
+		bullet.className = 'ermybullet';
+		bullet.style.top = tankDiv.offsetTop + 40 + 'px';
+		bullet.style.left = tankDiv.offsetLeft + 15 + 'px';
+		factory.appendChild(bullet);
+		var timer3 = setInterval(function() {
+			// var goDir = (tank.dir == 'bottom') ? top : left;
+			var m = bullet.offsetLeft;
+		    var x = Math.floor(m/60);
+			var n = bullet.offsetTop;
+			var y = Math.ceil(n/60)*10;
+			bullet.style.top = bullet.offsetTop + 10 + 'px';
+			// console.log(x,y);
+			if(n > 540) {
+				factory.removeChild(bullet);
+				bullet = null;
+				clearInterval(timer3);
+			}else if(tdList[x+y].className == 'block') {
+				factory.removeChild(bullet);
+				bullet = null;
+				tdList[x+y].className = 'default';
+				clearInterval(timer3);
+			}else if (tdList[x+y].className == 'wall') {
+                factory.removeChild(bullet);
+				bullet = null;
+				clearInterval(timer3);
+			}
+		},50);
+	}else{
+		var bullet = document.createElement('div');
+		bullet.className = 'ermybullet';
+		bullet.style.top = tankDiv.offsetTop + 15 + 'px';
+		bullet.style.left = tankDiv.offsetLeft + 40 + 'px';
+		factory.appendChild(bullet);
+		var timer3 = setInterval(function() {
+			// var goDir = (tank.dir == 'bottom') ? top : left;
+			var m = bullet.offsetLeft;
+		    var x = Math.ceil(m/60);
+			var n = bullet.offsetTop;
+			var y = Math.floor(n/60)*10;
+			bullet.style.left = bullet.offsetLeft + 10 + 'px';
+			// console.log(x,y);
+			if(m > 540) {
+				factory.removeChild(bullet);
+				bullet = null;
+				clearInterval(timer3);
+			}else if(tdList[x+y].className == 'block') {
+				factory.removeChild(bullet);
+				bullet = null;
+				tdList[x+y].className = 'default';
+				clearInterval(timer3);
+			}else if (tdList[x+y].className == 'wall') {
+                factory.removeChild(bullet);
+				bullet = null;
+				clearInterval(timer3);
+			}
+		},50);
+	}
 }
